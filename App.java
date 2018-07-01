4ac5fe48b09b5aad3343f52954343360a95c0b8c
Jhansi <ranijhansi.g@gmail.com>
2018-07-01 09:57:31 -0700

    Successful Git integration with Jenkins2

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App
 {
 public static void main(String []args){
 
-System.out.prinln("Hello. First program to integrate local Git with Jenkins");
+System.out.println("Hello. First program to integrate local Git with Jenkins");
 }
 
 }
